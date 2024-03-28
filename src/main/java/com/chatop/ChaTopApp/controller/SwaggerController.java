1658e8b2ddb4393e7064809f130ad21f91bd3429
Marushka Donskoff <marushkoff@gmail.com>
2024-03-28 17:34:01 +0100

    Swagger update

---
+++ src/main/java/com/chatop/ChaTopApp/controller/SwaggerController.java
@@ -0,0 +1,18 @@
+package com.chatop.ChaTopApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SwaggerController {
+    @RequestMapping("/documentation")
+    public String getRedirectHtml() {
+        return "redirect:swagger-ui.html";
+    }
+
+    @RequestMapping("/documentation-api")
+    public String getRedirectJsonUrl() {
+        return "redirect:v3/api-docs";
+    }
+
+}
